589f9cff0ea0c86d8d147b659060195e6e84be32
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-06-03 01:22:22 -0300

    Angular material agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { FormularioAComponent } from './formulario-a/formulario-a.component';
 import { FormularioBComponent } from './formulario-b/formulario-b.component';
 import { FormularioCComponent } from './formulario-c/formulario-c.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormularioCComponent } from './formulario-c/formulario-c.component';
     FormularioCComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
